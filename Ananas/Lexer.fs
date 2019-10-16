4393d0cf2da6503f26fcccc33ef5b012104fee2b
HubCodes <krlrhkstk12@gmail.com>
2019-10-16 10:27:58 +0900

    Add state transitions start from 'Start'

--- Ananas/Lexer.fs
+++ Ananas/Lexer.fs
@@ -1,5 +1,8 @@
 ﻿module Lexer
 
+open System
+open System.Text.RegularExpressions
+
 type LexerState =
     | Start
     | Name
@@ -7,4 +10,70 @@ type LexerState =
     | NumberInteger
     | NumberReal
 
-let rec lex (code: char list) state = ignore
+type Token =
+    | Name of string
+    | String of string
+    | Integer of int
+    | Double of double
+    | OpenParenthesis
+    | CloseParenthesis
+    | None
+
+let nameRegexPattern = new Regex("[a-zA-Z0-9\~\!\@\#\$\%\^\&\*_\-\+\=]", RegexOptions.Compiled)
+
+let isName (c: char) =
+    nameRegexPattern.Match(c.ToString()).Success
+
+let isQuote = function
+    | '"' -> true
+    | _ -> false
+
+let isSpace = Char.IsWhiteSpace
+
+let isDigit = Char.IsDigit
+
+let isParenthesis = function
+    | '('
+    | ')' -> true
+    | _ -> false
+
+let lex (code: char list) =
+    let mutable code = code
+    let mutable state = LexerState.Start
+    let mutable lexeme = ""
+    let mutable token = Token.None
+
+    let rec doLex () =
+        let head = List.head code
+        let tail = List.tail code
+
+        code <- tail
+
+        match (head, state) with
+        | (head, Start) when isName(head) ->
+            state <- LexerState.Name
+            lexeme <- head.ToString()
+            token <- Token.None
+            doLex ()
+        | (head, Start) when isSpace(head) ->
+            state <- LexerState.Start
+            lexeme <- ""
+            token <- Token.None
+            doLex ()
+        | (head, Start) when isQuote(head) -> 
+            state <- LexerState.String
+            lexeme <- ""
+            token <- Token.None
+            doLex ()
+        | (head, Start) when isDigit(head) ->
+            state <- LexerState.NumberInteger
+            lexeme <- head.ToString()
+            token <- Token.None
+            doLex ()
+        | (head, Start) when isParenthesis(head) ->
+            state <- LexerState.Start
+            lexeme <- "("
+            token <- Token.OpenParenthesis
+            token
+
+    doLex
--- Ananas/Program.fs
+++ Ananas/Program.fs
@@ -1,3 +1,7 @@
 ﻿[<EntryPoint>]
 let main argv =
+    let lexer = Lexer.lex <| Seq.toList "((("
+    printfn "%A" <| lexer ()
+    printfn "%A" <| lexer ()
+    printfn "%A" <| lexer ()
     0 // return an integer exit code
