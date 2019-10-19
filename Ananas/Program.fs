77421b4786cea6160c99b103615cc5b56829d1da
HubCodes <krlrhkstk12@gmail.com>
2019-10-19 14:48:04 +0900

    Add module TokenList
    * make Token list by using lexer fn

--- Ananas/Program.fs
+++ Ananas/Program.fs
@@ -1,17 +1,8 @@
 ﻿[<EntryPoint>]
 let main _ =
     let lexer = Lexer.lex "(+ i 3.14 \"Hello\" 12324) ((("
+    let tokenList = TokenList.makeTokenList lexer
 
-    let rec lexUntilEnd () =
-        let token = lexer ()
-
-        match token with
-        | Lexer.Token.None ->
-            printfn "End."
-        | _ ->
-            printfn "%A" token
-            lexUntilEnd ()
-
-    lexUntilEnd ()
+    tokenList |> printfn "%A"
 
     0
---
+++ Ananas/TokenList.fs
@@ -0,0 +1,11 @@
+﻿module TokenList
+
+open System
+
+let makeTokenList lexer =
+    let rec doMakeTokenList acc =
+        let nextToken = lexer ()
+        match nextToken with
+        | Lexer.Token.None -> List.rev acc
+        | _ -> doMakeTokenList <| nextToken :: acc
+    doMakeTokenList []
