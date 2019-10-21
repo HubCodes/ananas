a29dbfaaa3aa909741f574911c76cbef3472aa1e
HubCodes <krlrhkstk12@gmail.com>
2019-10-21 18:54:21 +0900

    Remove lexer and tokenList
    * Will use fslex and fsyacc

--- Ananas/Program.fs
+++ Ananas/Program.fs
@@ -1,8 +1,4 @@
 ﻿[<EntryPoint>]
 let main _ =
-    let lexer = Lexer.lex "(+ i 3.14 \"Hello\" 12324) ((("
-    let tokenList = TokenList.makeTokenList lexer
-
-    tokenList |> printfn "%A"
 
     0
