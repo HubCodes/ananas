9668fa8b2ea089324886ae55b774936ccfcc2a0f
HubCodes <krlrhkstk12@gmail.com>
2019-11-23 16:28:12 +0900

    Change parser to accept whitespace

--- Ananas/Parser.fs
+++ Ananas/Parser.fs
@@ -6,21 +6,26 @@ open AST
 
 #nowarn "40"
 
-let rec parseInt = pint32 |>> Int
+let skipWs = manyChars <| pchar ' '
 
-and parseBool = (pstring "true" <|> pstring "false") |>> Boolean.Parse |>> Bool
+let rec parseInt = pint32 .>> skipWs |>> Int
+
+and parseBool =
+    (pstring "true" <|> pstring "false") .>> skipWs |>> Boolean.Parse |>> Bool
 
 and normalChar = satisfy (fun c -> c <> '\\' && c <> '"')
 
 and parseString =
-    between (pstring "\"") (pstring "\"") (manyChars normalChar) |>> String
+    between (pstring "\"") (pstring "\"") (manyChars normalChar)
+    .>> skipWs
+    |>> String
 
 and parseLiteral = parseInt <|> parseBool <|> parseString
 
-and parseVar = identifier <| IdentifierOptions () |>> Var
+and parseVar = identifier <| IdentifierOptions () .>> skipWs |>> Var
 
 and parseFuncDec =
-    (pstring "\\" >>. parseVar) .>>. (pstring "->" >>. parseExpr) |>> FuncDec
+    (pstring "\\" >>. parseVar) .>>. (pstring "->" .>> skipWs >>. parseExpr) |>> FuncDec
 
 and parseExpr = parse {
     return! choice
